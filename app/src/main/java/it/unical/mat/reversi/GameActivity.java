827e8f15c1517f18835532c16c871e6aa6e11c1c
Stefano Germano <stefanogermano0@gmail.com>
2015-10-20 18:13:41 +0200

    New implementation of Game Layout

--- app/src/main/java/it/unical/mat/reversi/GameActivity.java
+++ app/src/main/java/it/unical/mat/reversi/GameActivity.java
@@ -1,9 +1,12 @@
 package it.unical.mat.reversi;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.Spinner;
 
 import it.unical.mat.reversi.ai.AIType;
@@ -41,6 +44,17 @@ public class GameActivity extends AppCompatActivity {
         if (playerTypeBlack == PlayerType.COMPUTER) {
             gameManager.setAI(getAIType(extraModeBlack.substring(extraModeBlack.indexOf("(") + 1,extraModeBlack.indexOf(")"))));
         }
+
+        for (int i = 1; i <= 8; i++)
+            for (int j = 1; j <= 8; j++) {
+                final ImageView a = (ImageView) findViewById(getResources().getIdentifier("c_" + i + "_" + j, "id", getPackageName()));
+                a.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Log.i("Clicked: ", String.valueOf(a.getId()));
+                    }
+                });
+            }
 
     }
 
