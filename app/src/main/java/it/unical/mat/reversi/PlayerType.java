8949c984fcd6bba803bf57070a557cbe08948d01
Stefano Germano <stefanogermano0@gmail.com>
2015-10-20 15:11:17 +0200

    Added Main Menu Activity

---
+++ app/src/main/java/it/unical/mat/reversi/PlayerType.java
@@ -0,0 +1,8 @@
+package it.unical.mat.reversi;
+
+/**
+ * Created by Stefano on 20/10/2015.
+ */
+public enum PlayerType {
+    HUMAN, COMPUTER
+}
--- app/src/main/java/it/unical/mat/reversi/ReversiActivity.java
+++ app/src/main/java/it/unical/mat/reversi/ReversiActivity.java
@@ -1,16 +1,40 @@
 package it.unical.mat.reversi;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class ReversiActivity extends AppCompatActivity {
+
+    public final static String EXTRA_MODE_WHITE = "it.unical.mat.reversi.MODE_WHITE";
+    public final static String EXTRA_MODE_BLACK = "it.unical.mat.reversi.MODE_BLACK";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reversi);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.player_mode, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        Spinner spinnerWhite = (Spinner) findViewById(R.id.spinnerPlayerWhite);
+        spinnerWhite.setAdapter(adapter);
+        Spinner spinnerBlack = (Spinner) findViewById(R.id.spinnerPlayerBlack);
+        spinnerBlack.setAdapter(adapter);
+
     }
 
     @Override
@@ -34,4 +58,15 @@ public class ReversiActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    public void startNewGame(View v) {
+
+        Intent intent = new Intent(this, GameActivity.class);
+        intent.putExtra(EXTRA_MODE_WHITE, (CharSequence) ((Spinner) findViewById(R.id.spinnerPlayerWhite)).getSelectedItem());
+        intent.putExtra(EXTRA_MODE_BLACK, (CharSequence) ((Spinner) findViewById(R.id.spinnerPlayerBlack)).getSelectedItem());
+        startActivity(intent);
+
+
+    }
+
 }
